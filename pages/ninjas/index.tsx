af12229f047225740ea23f8f502d5c453783a280
Adrian Mohmed Sanz <adrian.mohmed@kubide.es>
2021-07-13 16:47:41 +0200

    fetching users list from jsonPlaceHolder

--- pages/ninjas/index.tsx
+++ pages/ninjas/index.tsx
@@ -1,11 +1,27 @@
+import styles from '../../styles/Ninjas.module.css';
+
 export interface NinjasProps {
-    
+    ninjas: any
 }
- 
-const Ninjas: React.FunctionComponent<NinjasProps> = () => {
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+        props: { ninjas: data }
+    }
+}
+const Ninjas: React.FunctionComponent<NinjasProps> = ({ ninjas }) => {
     return ( 
         <div>
             <h1>All Ninjas</h1>
+            {ninjas.map( (ninja: any) => (
+                <div>
+                    <a className={styles.single}>
+                        <h3>{ninja.name}</h3>
+                    </a>
+                </div>     
+            ))}
         </div>
      );
 }
